b2f79e3a45049d9ea9ef4c9e71f964730c4038b4
TheTrueTostao <119060228+TheTrueTostao@users.noreply.github.com>
2023-03-14 15:50:45 +0100

    4 files modified

--- src/todo-list/todo-item/todo-item.component.ts
+++ src/todo-list/todo-item/todo-item.component.ts
@@ -1,16 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Todo } from '../../model/todo';
 
 @Component({
   selector: 'todo-item',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo-item.component.html',
   standalone: true,
   styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
-  constructor() {}
+  modification: boolean;
+  constructor() {
+    this.modification = false;
+  }
 
   @Input()
   todo: Todo;
@@ -20,6 +24,15 @@ export class TodoItemComponent implements OnInit {
 
   changeDone() {
     this.todo.done = !this.todo.done;
+    this.newEvent.emit(this.todo);
+  }
+
+  changeModifier() {
+    this.modification = true;
+  }
+
+  modifierLabel(nom: string) {
+    this.todo.label = nom;
     this.newEvent.emit(this.todo);
   }
 
