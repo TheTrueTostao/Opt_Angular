58a609184660da733b195bad3294b11d13d6d045
TheTrueTostao <119060228+TheTrueTostao@users.noreply.github.com>
2023-03-17 12:45:45 +0100

    3 files modified

--- src/todo-list/todo-item/todo-item.component.ts
+++ src/todo-list/todo-item/todo-item.component.ts
@@ -1,3 +1,5 @@
+declare var M: any;
+
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -34,6 +36,15 @@ export class TodoItemComponent implements OnInit {
   modifierLabel(nom: string) {
     this.todo.label = nom;
     this.newEvent.emit(this.todo);
+  }
+
+  closeEdit(event: any) {
+    this.modification = false;
+    this.newEvent.emit(this.todo);
+  }
+
+  showUserNotification(todo: Todo) {
+    M.toast({ html: `${this.todo.label} a été mis à jour` });
   }
 
   ngOnInit() {}
